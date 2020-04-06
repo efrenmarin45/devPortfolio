45daf1dc05983c381da9595e1be223eef377c6cc
Efren_Marin <efren45marin@gmail.com>
2020-04-05 23:49:59 -0700

    Added separate sections of the portfolio. Included contact form, gradient colors, partial styling, navbar, and more changes.

--- js/script.js
+++ js/script.js
@@ -4,18 +4,25 @@ const typewriter = new Typewriter('#typewriter', {
   });
 
   typewriter.typeString('<style="text-align: center">Hello, I\'m Efren Marin')
-    .pauseFor(2500)
+    .pauseFor(2000)
     .typeString('<br><style="text-align: center">I\'m a Front-end developer')
-    .pauseFor(2500)
+    .pauseFor(2000)
     .changeDeleteSpeed(5)
     .deleteChars(19)
     .typeString('Back-end developer')
-    .pauseFor(2500)
+    .pauseFor(2000)
     .deleteChars(19)
-    .typeString('<strong> Full-Stack developer!<strong>')
-    .pauseFor(2500)
+    .typeString('<strong> Full-Stack developer<strong>')
+    .pauseFor(4500)
     .deleteAll(5)
-    .typeString('Welcome!')
-    .pauseFor(10000)
+    .typeString('Let\'s Build Something Together')
+    .pauseFor(7000)
     .deleteAll(5)
     .start();
+
+
+
+$(window).on('beforeunload', function() {
+  $('body').hide();
+  $(window).scrollTop(0);
+});
